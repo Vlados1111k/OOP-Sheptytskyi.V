fca04cccb59a4b005c0f764e89586cc3ee802916
Vlados1111k <sheptytskyi.v_ipz23@rcit.ukr.education>
2026-05-06 23:52:13 +0300

    Delete CinemaPeak directory

--- CinemaPeak/src/Cinema.Application/IRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace Cinema.Application.Interfaces;
-
-public interface IRepository<T>
-{
-    IReadOnlyCollection<T> GetAll();
-    void Add(T entity);
-    Task SaveAsync();
-    Task LoadAsync();
-}
